244974d2c7db8e75366d60c0d7738292ce49cc92
Max Krieg <maxwell.krieg@gmail.com>
2021-01-01 11:37:46 -0700

    add user to user home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,13 @@
 import Container from '@material-ui/core/Container'
 import React from 'react'
+import { useUser } from '../UserContext'
 
 export const Home: React.FC = () => {
+  const { user } = useUser()
+
   return (
     <Container component="main" maxWidth="lg">
-      Home
+      User Home {user!.firstName}
     </Container>
   )
 }
